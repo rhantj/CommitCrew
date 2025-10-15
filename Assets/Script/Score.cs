75022e27a77ec9e225a172660b848f1d05065303
nuclear135 <nuclear135531@gmail.com>
2025-10-16 00:03:59 +0900

    ui&gamemanager First

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = score.ToString();
+    }
+}
